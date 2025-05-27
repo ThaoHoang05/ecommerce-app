f589190a59a1f3831178f1891176cb111f2b5d1b
tramdao2102 <tram.db235848@sis.hust.edu.vn>
2025-05-27 22:50:29 +0700

    update

--- src/main/java/com/stationeryshop/model/Category.java
+++ src/main/java/com/stationeryshop/model/Category.java
@@ -1,4 +1,4 @@
-package model;
+package com.stationeryshop.model;
 
 public class Category {
     private int categoryId;
--- src/main/java/com/stationeryshop/model/Product.java
+++ src/main/java/com/stationeryshop/model/Product.java
@@ -1,4 +1,4 @@
-package model;
+package com.stationeryshop.model;
 
 import java.time.LocalDateTime;
 
