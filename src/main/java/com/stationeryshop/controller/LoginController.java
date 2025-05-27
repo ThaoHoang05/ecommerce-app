f90b1f016b1232567991072b4724eeb7486e919e
thao <email>
2025-05-27 16:33:22 +0700

    sua loi

--- src/main/java/com/stationeryshop/controller/LoginController.java
+++ src/main/java/com/stationeryshop/controller/LoginController.java
@@ -1,4 +1,19 @@
 package com.stationeryshop.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField tfUsername;
+
+    @FXML
+    private TextField tfPwd;
+
+    @FXML
+    void btnLoginPressed(ActionEvent event) {
+
+    }
 }
--- src/main/java/com/stationeryshop/controller/SignupController.java
+++ src/main/java/com/stationeryshop/controller/SignupController.java
@@ -1,4 +1,31 @@
 package com.stationeryshop.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignupController {
+
+    @FXML
+    private PasswordField pfPassword;
+
+    @FXML
+    private TextField tfName;
+
+    @FXML
+    private TextField tfEmail;
+
+    @FXML
+    private TextField tfLoginName;
+
+    @FXML
+    void btnAcceptCreate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignupPressed(ActionEvent event) {
+
+    }
 }
