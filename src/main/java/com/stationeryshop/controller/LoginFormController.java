8a7039b88a4a3d273a3b5d91ec9f1378aba03042
thao <email>
2025-05-28 15:47:16 +0700

    commit

--- src/main/java/com/stationeryshop/controller/LoginFormController.java
+++ src/main/java/com/stationeryshop/controller/LoginFormController.java
@@ -1,12 +1,12 @@
 package com.stationeryshop.controller;
 
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
-import javax.swing.*;
 
 public class LoginFormController {
 
@@ -25,7 +25,9 @@ public class LoginFormController {
     @FXML
     void handleLogin(ActionEvent event) {
         int result = new UserController().handleLogin(usernameField.getText(), passwordField.getText());
-        if(result == 1) JOptionPane.showMessageDialog(null, "Login Successful");
+        if(result == 1){ 
+            JOptionPane.showMessageDialog(null, "Login Successful");
+        }
         else if(result == 0) JOptionPane.showMessageDialog(null, "Wrong Password","ERROR",JOptionPane.ERROR_MESSAGE);
         else JOptionPane.showMessageDialog(null, "User Not Found","ERROR",JOptionPane.ERROR_MESSAGE);
     }
