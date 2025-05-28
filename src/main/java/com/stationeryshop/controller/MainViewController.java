8160390ede7fc996fb77a555e6062be2e66b3309
nhidh235802 <duongnhi2932@gmail.com>
2025-05-29 00:11:31 +0700

    Report fxml + skeleton controller

--- src/main/java/com/stationeryshop/controller/MainViewController.java
+++ src/main/java/com/stationeryshop/controller/MainViewController.java
@@ -3,17 +3,38 @@ package com.stationeryshop.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 
 public class MainViewController {
+
+    @FXML
+    private HBox inventoryTab;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private StackPane ContentArea;
+
+    @FXML
+    private TextField searchitemField;
+
+    @FXML
+    private Label customerName;
 
     @FXML
     private Button signupBtn;
 
     @FXML
-    private Button loginBtn;
+    private HBox reportTab;
+
+    @FXML
+    private ImageView customerAvatar;
 
     @FXML
     private HBox AccountHbox;
@@ -22,20 +43,50 @@ public class MainViewController {
     private Region spacer;
 
     @FXML
+    private HBox supplierTab;
+
+    @FXML
+    private Button AIChatbotBtn;
+
+    @FXML
+    private HBox cartTab;
+
+    @FXML
     private HBox LoginHbox;
 
     @FXML
-    private ImageView customerAva;
+    void gotoCartForm(ActionEvent event) {
 
+    }
+
+    @FXML
+    void gotoProductForm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gotoSupplierForm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gotoReportForm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gotoAIChatbot(ActionEvent event) {
+
+    }
 
     @FXML
     void gotoLoginForm(ActionEvent event) {
 
     }
 
-
     @FXML
     void gotoSignupForm(ActionEvent event) {
 
     }
+
 }
