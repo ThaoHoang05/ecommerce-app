da7266cdccecab118ee683f47dd77a0b475c2fa4
thao <email>
2025-06-04 00:12:49 +0700

    update inventoryProduct

--- src/main/java/com/stationeryshop/controller/inventory/ProductController.java
+++ src/main/java/com/stationeryshop/controller/inventory/ProductController.java
@@ -1,29 +1,4 @@
 package com.stationeryshop.controller.inventory;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-
 public class ProductController {
-
-    @FXML
-    private Button refreshBtn;
-
-    @FXML
-    private TextField searchField;
-
-    @FXML
-    private Button searchBtn;
-
-    @FXML
-    void handleSearch(ActionEvent event) {
-
-    }
-
-    @FXML
-    void handleRefresh(ActionEvent event) {
-
-    }
-
 }
