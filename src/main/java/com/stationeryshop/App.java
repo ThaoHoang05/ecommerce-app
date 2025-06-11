b5a7ed18ea8ce6d006057362e29ad5f755c1ae21
thao <thaohoang5836@gmail.com>
2025-06-11 20:30:04 +0700

    thu dong goi jar

---
+++ src/main/java/com/stationeryshop/App.java
@@ -0,0 +1,30 @@
+package com.stationeryshop;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        try{
+            final String MAINVIEW_PATH = "/fxml/MainView.fxml";
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(MAINVIEW_PATH));
+            Parent root = loader.load();
+            primaryStage.setTitle("Stationery Shop");
+            primaryStage.setScene(new Scene(root));
+
+            primaryStage.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
